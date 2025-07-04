8822b690077a8d764ba63c05828e66151b4aedc5
Helene Nguyen <helene.nguyen@filigran.io>
2025-07-04 08:53:04 +0200

    [connectors] refacto script + change init config

--- shared/tools/composer/generate_manifest.py
+++ shared/tools/composer/generate_manifest.py
@@ -2,31 +2,29 @@ import json
 import os
 from pathlib import Path
 
-# filename = "generator_tmp.py"
-filepath = os.path.dirname(__file__)
-print(filepath)
-parent = Path(__file__).parents[3]
-print("PARENT", parent)
 get_version = os.getenv("CIRCLE_TAG")
 
 manifest = {"name": "OpenCTI Connectors contracts", "contracts": []}
 
-# test add in manifest
+# Find all contracts
 json_files = []
 
 for root, dirs, files in os.walk("."):
     for file in files:
         if file.endswith("connector_schema.json"):
             json_files.append(os.path.join(root, file))
-print(json_files)
 
+# Add in manifest.json file
 for connector_contract in json_files:
     with open(connector_contract, encoding="utf-8") as file:
         test = json.load(file)
         manifest["contracts"].append(test)
 
+# Format manifest
 manifest = json.dumps(manifest, indent=2)
 
-manifest_path = os.path.join(parent, "manifest.json")
+# Write and add manifest file in root
+connector_root_path = Path(__file__).parents[3]
+manifest_path = os.path.join(connector_root_path, "manifest.json")
 with open(manifest_path, "w", encoding="utf-8") as manifest_file:
     manifest_file.write(manifest)
--- shared/tools/composer/manifest_generator.sh
+++ shared/tools/composer/manifest_generator.sh
@@ -2,12 +2,13 @@
 
 set -e  # exit on error
 
-# Method to activate venv
 activate_venv() {
+    # Method to activate isolate venv
+
     # Install dependencies
     requirements_file=$(find "$1" -name "requirements.txt")
 
-    # Create isolated virtual environment
+    # Create isolated virtual environment in connector path
     venv_name=".temp_venv"
     echo '> Creating isolated virtual environment...'
     python -m venv "$1/$venv_name"
@@ -24,6 +25,7 @@ activate_venv() {
     # -qq: Hides both informational and warning messages, showing only errors.
     python -m pip install -qq -r "$requirements_file"
 
+    # Check if venv is well created
     venv_exists=$(find "$1" -name ".temp_venv")
 
     if [ -d "$venv_exists" ]; then
@@ -34,17 +36,19 @@ activate_venv() {
 }
 
 deactivate_venv() {
-      echo -e "\n> Clean Up environment..."
-      deactivate
-      rm -rf "$1"
+    # Method to deactivate venv and remove the folder
+    echo -e "\n> Clean Up environment..."
+    deactivate
+    echo "> Removing temp venv..."
+    rm -rf "$1"
 }
 
-## Find all parents directory of connector with config loader
-## printf action with the %h format specifier, which prints the directory part (parent directory) of the file path
+# Find all parents directory of connector with config loader
+# printf action with the %h format specifier, which prints the directory part (parent directory) of the file path
 connector_directories_path=$(find . -name "__infos__" -printf '%h\n')
 echo -e "Generate contract for: " "\n$connector_directories_path"
 
-## Loop in each connector directory with infos
+# Loop in each connector directory with infos
 for connector_directory_path in $connector_directories_path
 do
   if [ -d "$connector_directory_path" ]; then
@@ -56,17 +60,16 @@ do
     # apply generator
     python "$connector_directory_path""/generator_tmp.py"
 
-    # TODO steps
-    # clean up
-    # remove tmp_schema_generator
-    rm "$connector_directory_path""/generator_tmp.py"
+    # Clean up
+    # Remove tmp_schema_generator
+    rm "$connector_directory_path/generator_tmp.py"
 
     deactivate_venv "$connector_directory_path/$venv_name"
   fi
 done
 
 generate_manifest=$(find . -name "generate_manifest.py")
-echo "$generate_manifest"
+echo -e "\nGenerating manifest file..."
 python "$generate_manifest"
 
 
